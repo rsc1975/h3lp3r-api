7b0879d21a25c0c38e52ce6f4ee541dd49159357
Roberto Sánchez <rsanchez@qualityobjects.com>
2022-09-05 18:00:31 +0200

    Added middleware regexp

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ export const app = new Hono({"strict": true});
 const usemicros = (await Deno.permissions.query({ name: "hrtime"})).state === "granted";
 const quiet = Deno.env.get("QUIET") === "true";
 
-app.use(CONTEXT_PATH, async (c: Context, next: Next) => {
+app.use(`${CONTEXT_PATH}/*`, async (c: Context, next: Next) => {
     const start = performance.now();
     await next(); 
     
@@ -31,12 +31,12 @@ app.use(CONTEXT_PATH, async (c: Context, next: Next) => {
     }
 });
 
-app.use(CONTEXT_PATH, cors())
+app.use(`${CONTEXT_PATH}/*`, cors())
 
 // This is an optimization to avoid the call to c.req.query
 const hasSearchParams = (req: Request) => req.url.indexOf('?', 10) > -1;
 
-app.use(CONTEXT_PATH, async (c: Context, next: Next) => {
+app.use(`${CONTEXT_PATH}/*`, async (c: Context, next: Next) => {
     const pretty = !!(hasSearchParams(c.req) && (c.req.query(PRETTY_PARAM) || c.req.query(PRETTY_PARAM) === ''));
     c.pretty(pretty, 2);
     await next();     
